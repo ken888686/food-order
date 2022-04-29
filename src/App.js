a58bcd387e1a6f82f54b9a60109c449cae975537
Aaron <ken888686@gmail.com>
2022-04-29 21:51:09 +0800

    event

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ function App() {
   const expenses = [
     {
       id: 1,
-      title: 'Car Insurance',
+      title: 'Car A',
       amount: 100,
       date: new Date(2020, 0, 1),
     },
     {
       id: 2,
-      title: 'Car Insurance',
+      title: 'Car B',
       amount: 200,
       date: new Date(2022, 1, 4),
     },
     {
       id: 3,
-      title: 'Car Insurance',
+      title: 'Car C',
       amount: 300,
       date: new Date(2022, 2, 4),
     },
     {
       id: 4,
-      title: 'Car Insurance',
+      title: 'Car D',
       amount: 400,
       date: new Date(2022, 3, 4),
     },
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import './ExpenseItem.scss';
 
 export default function ExpenseItem(props) {
   const { title, amount, date } = props;
+  const clickHandler = (input) => {
+    console.log(input);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -14,6 +17,9 @@ export default function ExpenseItem(props) {
           {amount}
         </p>
       </div>
+      <button type="button" onClick={() => clickHandler(title)}>
+        Change Title
+      </button>
     </Card>
   );
 }
