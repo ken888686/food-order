8c4104af916f1825c68379e92a086957678a9642
Aaron <ken888686@gmail.com>
2022-05-01 16:11:28 +0800

    input debounce & clear timer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ function Login(props) {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6,
-    );
+    const timer = setTimeout(() => {
+      console.log('formIsValid');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6,
+      );
+    }, 500);
+
+    return () => {
+      console.log('clear timer');
+      clearTimeout(timer);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
