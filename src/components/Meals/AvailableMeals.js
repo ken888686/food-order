8c9ae0558613d8e76e8b74a57bb59b803f40477c
Aaron <ken888686@gmail.com>
2022-05-01 23:09:01 +0800

    add AvailableMeals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,16 +1,27 @@
 import DUMMY_MEALS from './dummy-meals';
 import classes from './AvailableMeals.module.scss';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 export default function AvailableMeals() {
   const mealsList = DUMMY_MEALS.map(
-    (meal) => <li key={meal.id}>{meal.name}</li>,
+    (meal) => (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    ),
   );
 
   return (
     <section className={classes.meals}>
-      <ul>
-        {mealsList}
-      </ul>
+      <Card>
+        <ul>
+          {mealsList}
+        </ul>
+      </Card>
     </section>
   );
 }
