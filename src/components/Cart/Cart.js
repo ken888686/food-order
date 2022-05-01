9d922af6b267e87e5a5a177814086f603a84757f
Aaron <ken888686@gmail.com>
2022-05-02 01:47:00 +0800

    add CartItem

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,31 @@
+/* eslint-disable react/jsx-no-bind */
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.scss';
+import CartItem from './CartItem';
 
 export default function Cart(props) {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => { console.log(id); };
+
+  const cartItemAddHandler = (item) => { console.log(item); };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{
-        id: 'c1',
-        name: 'Chicken Burger',
-        amount: 2,
-        price: 10,
-      }].map((item) => <li key={item.id}>{item.name}</li>)}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
     </ul>
   );
 
@@ -18,7 +34,7 @@ export default function Cart(props) {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>200</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button
@@ -28,12 +44,14 @@ export default function Cart(props) {
         >
           Close
         </button>
+        {hasItems && (
         <button
           type="button"
           className={classes.button}
         >
           Order
         </button>
+        )}
       </div>
     </Modal>
   );
