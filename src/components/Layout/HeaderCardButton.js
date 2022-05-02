55ac92a7eb88c5eba504283aeeb4792c365c1137
Aaron <ken888686@gmail.com>
2022-05-02 13:35:33 +0800

    header button animation

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,20 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.scss';
 
 export default function HeaderCardButton(props) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce(
+  const numberOfCartItems = items.reduce(
     (curr, item) => curr + item.amount,
     0,
   );
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return undefined;
+    }
+
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <button
+      className={btnClasses}
       type="button"
-      className={classes.button}
       onClick={props.onClick}
     >
       <span className={classes.icon}>
